b491bd27cb464deb378aaae21bf5cedcd88ea6dc
Moncef <moncef.koumila@gmail.com>
2020-12-26 20:41:14 +0000

    Make API request from Wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = React.useState("");
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  // Make an API request from Wikipedia first time the component is rendered & whenever we change 'term'
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      // Set the returned data from the API request to results array
+      setResults(data);
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
