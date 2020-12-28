5e1c17fa73433221d498dcedd89dd7fbf3893ce8
Moncef <moncef.koumila@gmail.com>
2020-12-28 15:01:00 +0000

    Hiding and showing dropdown's option list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  // State to track if the dropdown is open or close
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = options.map((option) => {
     // Hide the selected color from the dropdown options
     if (option.value === selected.value) {
@@ -22,10 +25,15 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
