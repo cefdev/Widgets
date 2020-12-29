850ddae5434f4224675523f1494b4f230838527f
Moncef <moncef.koumila@gmail.com>
2020-12-29 19:33:02 +0000

    Fix a bug

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -21,6 +21,7 @@ const Translate = () => {
   return (
     <div>
       <Dropdown
+        label="Select a language"
         selected={language}
         onSelectedChange={setLanguage}
         options={options}
