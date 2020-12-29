a86ae8a979da91ace1b1a33aaf89b544258b2669
Moncef <moncef.koumila@gmail.com>
2020-12-29 20:41:59 +0000

    Displaying translated text in UI

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,20 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  // To save the returned results from the API request
+  const [translated, setTranslated] = useState("");
+
+  // Make an API request when the app start and when 'language' and 'text' changed
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-        },
-      }
-    );
+    const doTranslation = async () => {
+      // Get only 'data' from the request's response
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+
+      // Set result to 'translated' variable
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    // Call it to make the API request
+    doTranslation();
   }, [language, text]);
-  return <div></div>;
+
+  return (
+    <div>
+      <h2 className="ui header">{translated}</h2>
+    </div>
+  );
 };
 
 export default Convert;
