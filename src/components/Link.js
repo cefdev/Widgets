43abebc6e4155cffece930423bf145d20e164c85
Moncef <moncef.koumila@gmail.com>
2020-12-30 19:19:38 +0000

    Detecting the URL has changed

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Link = ({ className, href, children }) => {
-  // Change the URL whenever the user click on a link from the navigation BUT DO NOT REFRESH THE PAGE
   const onClick = (e) => {
     e.preventDefault();
+
+    // Change the URL whenever the user click on a link from the navigation BUT DO NOT REFRESH THE PAGE
     window.history.pushState({}, "", href);
+
+    // To tell the Route component that the URL has changed
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,19 @@
-// Any component inside this Route component is considered as children, That's why we used the word 'children' as a parameter
+import { useEffect } from "react";
 
 const Route = ({ path, children }) => {
+  // Detect that the URL has changed
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log("Location Changed");
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+
+    return () => {
+      window.removeEventListener("popstate", onlanguagechange);
+    };
+  }, []);
+
   return window.location.pathname === path ? children : null;
 };
 
